694964816ac59950510a455d5eb65c1a2c80742c
pranavanba <pranavanba@gmail.com>
2023-07-13 21:42:06 +0000

    Add arrow package

--- install_requirements.R
+++ install_requirements.R
@@ -10,3 +10,5 @@ install.packages("tools")
 install.packages("reticulate")
 install.packages("synapser", repos=c("http://ran.synapse.org", "http://cran.fhcrc.org"))
 install.packages("synapserutils", repos=c("http://ran.synapse.org", "http://cran.fhcrc.org"))
+install.packages("arrow")
+
