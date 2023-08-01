7f05aa971a9d624bc39b98026bed908e35922651
Pranav Anbarasu <pranavanba@gmail.com>
2023-08-01 18:36:00 +0000

    Update params

--- sts_params_external.R
+++ sts_params_external.R
@@ -1,11 +1,11 @@
 # Parameters for sts_synindex.R process
 
 # S3 bucket containing the post-ETL parquet datasets
-# PARQUET_BUCKET <- 'recover-processed-data'
+PARQUET_BUCKET <- 'recover-processed-data'
 
-# PARQUET_BUCKET_BASE_KEY <- 'main/parquet/'
+PARQUET_BUCKET_BASE_KEY <- 'main/parquet/'
 
-PARQUET_FOLDER <- 'syn52138259'
+PARQUET_INTERNAL_FOLDER <- 'syn51406699'
 
 # Local location where parquet bucket files are synced to
 AWS_PARQUET_DOWNLOAD_LOCATION <- './temp_aws_parquet'
@@ -31,7 +31,7 @@ cols_to_drop <- list(c("EmailAddress", "DateOfBirth", "CustomFields_DeviceOrderI
 PARQUET_FINAL_LOCATION <- './parquet_final'
 
 # Synapse location where the S3 bucket objects are listed
-SYNAPSE_PARENT_ID <- 'syn52138259'
+SYNAPSE_PARENT_ID <- 'syn52224315'
 
 # synID of the file view containing a list of all currently indexed S3 objects from the parquet bucket folder in Synapse
-# SYNAPSE_FILEVIEW_ID <- 'syn52133651'
+SYNAPSE_FILEVIEW_ID <- 'syn52146296'
